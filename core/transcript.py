fc5dc2970eafaebf971f11546b030ea8d57d6db6
ShubhSarin <shubhsarin08@gmail.com>
2026-03-16 19:55:01 +0530

    added proxy configuration to transcript fetching and updated requirements.txt with new dependencies.

--- core/transcript.py
+++ core/transcript.py
@@ -1,6 +1,13 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import urlparse, parse_qs
+from youtube_transcript_api.proxies import WebshareProxyConfig
 
+api = YouTubeTranscriptApi(
+    proxy_config=WebshareProxyConfig(
+        proxy_username="pftpopbe",
+        proxy_password="3p0xhoa5aatg",
+    )
+)
 
 def get_video_id(youtube_url: str) -> str:
     parsed = urlparse(youtube_url)
@@ -15,6 +22,5 @@ def get_video_id(youtube_url: str) -> str:
 
 
 def extract_transcript_from_id(video_id: str) -> str:
-    api = YouTubeTranscriptApi()
     transcript = api.fetch(video_id)
     return " ".join(chunk.text for chunk in transcript)
